1a0534f5f5b1da3741c34e714e5ce6d25bd0e8ee
Minecraftjavafa <95871802+Minecraftjavafaniscool90@users.noreply.github.com>
2022-02-02 17:24:17 +0000

    I made a separate file separating from the old script im testing, and trying to see if I can fix an error.

---
+++ main (original test code).py
@@ -0,0 +1,9 @@
+answer = input ("Hello, woud you like to play the game? (yes/no)")
+
+if answer.lower().strip == "yes":
+  
+ 
+   answer = input("Ok, so.. what type of adventure you want")
+  
+else:
+   print("You got an ending: failed to choose a text adventure game.")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 answer = input ("Hello, woud you like to play the game? (yes/no)")
 
 if answer.lower().strip == "yes":
-  else:
-      print("You got the worse ending there is!")
+  
+ 
+   answer = print("Ok, so.. what type of adventure you want")
+  
